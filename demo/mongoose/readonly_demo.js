3bd3abac6b1782846b6eb7db9917e055dbfaafd4
Juho Vepsäläinen <bebraw@gmail.com>
2012-10-27 14:42:38 +0300

    Add readonly demo

---
+++ demo/mongoose/readonly_demo.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+var express = require('express');
+var mongoose = require('mongoose');
+var sugar = require('mongoose-sugar');
+var rest = require('../../lib/rest-sugar');
+var models = require('./models');
+
+main();
+
+function main() {
+    mongoose.connect('mongodb://localhost/mongoosedemo');
+
+    var app = express.createServer();
+
+    app.configure(function() {
+        app.use(express.methodOverride()); // handles PUT
+        app.use(express.bodyParser()); // handles POST
+        app.use(app.router);
+    });
+
+    rest.init(app, '/api/v1/', {
+        'libraries': models.Library,
+        'tags': models.Tag
+    }, sugar, rest.auth.only('GET'));
+
+    console.log('Surf to localhost:8000');
+    app.listen(8000);
+}
+
